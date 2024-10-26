50223a13cfc630b236d8bc9a8630ddab31953b4f
Maxence Choiselle <mchoiselle@gmail.com>
2024-10-26 11:00:05 +0200

    [Feat] : complete APi employee button

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 
+import { useState } from 'react';
 import './App.css'
 import EmployeeCard from './EmployeeCard'
 const sampleEmployee = {
@@ -13,13 +14,22 @@ const sampleEmployee = {
 };
 
 function App() {
-  
+  const [employee, setEmployee] = useState(sampleEmployee);
+  const getEmployee = () => {
+    // Send the request
+    fetch("https://randomuser.me/api?nat=en")
+      .then((response) => response.json())
+      .then((data) => {
+        setEmployee(data.results[0]);
+      });
+  };
 
   return (
-    <>
-     <EmployeeCard employee={sampleEmployee} />
-    </>
-  )
+    <div className='App'>
+      <EmployeeCard employee={employee} />
+      <button type="button" onClick={getEmployee}>Get employee</button>
+    </div>
+  );
 }
 
 export default App
